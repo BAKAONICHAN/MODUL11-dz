c47914f44bee909512bcdd7b76c4a39e8de9d0d6
Nurik <tanatov1989@mail.ru>
2017-12-27 16:31:44 +0600

    Добавьте файлы проекта.

---
+++ modul11/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace modul11
+{
+    public struct Employee
+    {
+        public string Name { get; set; }
+        public Vacancies Vacancie { get; set; }
+        public int Payment { get; set; }
+        public DateTime dateOfGettingTheJob { get; set; }
+
+
+        public Employee(string name, int payment, Vacancies vacancie, DateTime _dateOfGettingTheJob)
+        {
+            Name = name;
+            Payment = payment;
+            Vacancie = vacancie;
+            dateOfGettingTheJob = _dateOfGettingTheJob;
+        }
+        public void show(int numberOfEmployee)
+        {
+            Write(numberOfEmployee+":");
+            WriteLine("Имя"+Name);
+            WriteLine("Зарплата"+Payment);
+            WriteLine("Вакансия"+Vacancie);
+            WriteLine("Дата приема на работу"+dateOfGettingTheJob);
+        }
+    }
+}
---
+++ modul11/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace modul11
+{
+
+        class Program
+        {          
+           static void Main(string[] args)
+        { 
+                int mainPayment = 0;
+                int length = 0;
+                string nameOfEmployee;
+                int paymentOfEmployee;
+                string vacancie;
+                int counterOfBosses = 0;
+                int countOfClerks = 0;
+                DateTime dateOfGettingTheJob;
+                DateTime dateOfGettingTheJobByBoss=DateTime.Now;
+                
+            /////////////////////////////////////////////////
+            WriteLine("Введите кол-во сотрудников ");
+                while (!int.TryParse(ReadLine(), out length))
+                {
+                    Clear();
+                    Write("Ошибка, введите целое число: ");
+                }
+                int[] ArrayOfEmployees = new int[length];
+                Employee[] employeers = new Employee[length];
+                WriteLine("Заполните поля для получения информации о сотруднике.");
+                for (int i = 0; i < ArrayOfEmployees.Length; i++)
+                {
+                   
+                    WriteLine("Введите имя сотрудника.");
+                    nameOfEmployee = ReadLine();
+                    WriteLine("Введите зарплату сотрудника.");
+                    while (!int.TryParse(ReadLine(), out paymentOfEmployee))
+                    {
+                        Clear();
+                        Write("Ошибка, введите целое число: ");
+                    }
+                WriteLine("Введите дату получения работы этим сотрудником");
+                while (!DateTime.TryParse(ReadLine(), out dateOfGettingTheJob))
+                {
+                    Clear();
+                    Write("Ошибка, введите дату: ");
+                }
+                WriteLine("Введите вакансию сотрудника.");
+                    vacancie = ReadLine();
+                    if (vacancie == "Boss")
+                    {
+                        if (counterOfBosses >= 1)
+                        {
+                            WriteLine("Error(Босс только один.)");
+                            break;
+                        }
+                        else
+                        {
+                        
+                            employeers[i] = new Employee(nameOfEmployee, paymentOfEmployee, Vacancies.Boss,dateOfGettingTheJobByBoss);
+                        
+                            counterOfBosses++;
+                        }
+                    }
+                    else if (vacancie == "Clerk")
+                    {
+                        employeers[i] = new Employee(nameOfEmployee, paymentOfEmployee, Vacancies.Clerk, dateOfGettingTheJob);
+                    }
+                    else if (vacancie == "Manager")
+                    {
+                        employeers[i] = new Employee(nameOfEmployee, paymentOfEmployee, Vacancies.Manager, dateOfGettingTheJob);
+                    }
+                Clear();
+                }
+            
+                //Вывод сотрудников
+                ///////////////////////////
+                for (int j = 0; j < employeers.Length; j++)
+                {
+                    employeers[j].show(j);
+                }
+                //////////////////////////
+                for (int i = 0; i < employeers.Length; i++)
+                {
+
+                    if (employeers[i].Vacancie == Vacancies.Clerk)
+                    {
+                        mainPayment = mainPayment + employeers[i].Payment;
+                    countOfClerks++;
+                    }
+
+
+                }
+                mainPayment = mainPayment / countOfClerks;//Средняя зарплата
+            ////////////////////////////////////////////////////
+                Write("Средняя зарплата клерков:" + mainPayment);
+                for (int i = 0; i < employeers.Length; i++)
+                {
+                    if (employeers[i].Vacancie == Vacancies.Manager)
+                    {
+                        if (employeers[i].Payment > mainPayment)
+                        {
+                            employeers[i].show(i);
+                        }
+                    }
+
+                }
+            WriteLine(dateOfGettingTheJobByBoss);
+                ///////////////////////////////////////////////////////
+                for(int i=0;i<employeers.Length;i++)
+                {
+                if(employeers[i].dateOfGettingTheJob<dateOfGettingTheJobByBoss)
+                {
+                    employeers[i].show(i);
+                }
+                }
+                ReadLine();
+
+            }
+        }
+    }
+
+  
+
